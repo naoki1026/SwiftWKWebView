456471d73626587f258898c9cd9e3b458de207f3
Naoki Arakawae <naoki.arakawa1026@gmail.com>
2019-02-18 17:31:53 +0900

    first committ

--- SwiftWKWebView/ViewController.swift
+++ SwiftWKWebView/ViewController.swift
@@ -7,14 +7,88 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    var urlString : String = "https://www.1101.com/home.html"
+    var indicator : UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setIndicator()
+        
+        guard let url = URL (string: urlString) else {
+            
+            return
+        }
+        
+        let req = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 100)
+        
+        self.webView.load(req)
+        
+        
     }
-
-
+    
+    @IBAction func back(_ sender: Any) {
+        
+        webView.goBack()
+    }
+    
+    
+    
+    @IBAction func forward(_ sender: Any) {
+        
+        webView.goForward()
+    }
+    
+    @IBAction func refresh(_ sender: Any) {
+        
+        webView.reload()
+        
+    }
+    
+    
+    func setIndicator() {
+        
+        //インディケーターの初期化を行わなくてはいけない
+        indicator = UIActivityIndicatorView()
+        
+        
+        //サイズを決定する
+        indicator.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        indicator.center = CGPoint(x: UIScreen.main.bounds.size.width/2, y: UIScreen.main.bounds.size.height/2)
+        
+        //アニメーションが止まっている時の表示
+        indicator.hidesWhenStopped = true
+        
+        //indicatorの色について
+        indicator.style = .gray
+        
+        //WebViewにaddSubView
+        self.webView.addSubview(indicator)
+        
+    }
+    
+    //通信が開始したタイミングで呼ばれる
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+    
+        //インディケーターを回したい
+        indicator.startAnimating()
+        
+        
+    }
+    
+    //通信が完了したタイミングで呼ばれる
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        indicator.stopAnimating()
+    }
+    
+    
 }
-
